46ea1ac9a511f0c5365de60af3558631d6992145
Aditya Satya <73811047+imAdityaSatya@users.noreply.github.com>
2024-07-29 22:28:45 +0530

    Update RemoveDuplicates.java

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    String removeDups(String str) {
+        // code here
+        String s = "";
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(!s.contains(String.valueOf(ch))){
+                s+=ch;
+            }
+        }
+        return s;
+    }
+}
