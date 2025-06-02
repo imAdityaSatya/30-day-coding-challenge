cbdf897014eaf115a358d6a1ce32320b8c2de2b4
Aditya Satya <73811047+imAdityaSatya@users.noreply.github.com>
2025-06-02 12:28:38 +0530

    Create MinimumJumps.java

---
+++ MinimumJumps.java
@@ -0,0 +1,25 @@
+
+class MinimumJumps {
+    static int minJumps(int[] arr) {
+        // code here
+        int max=0, jump=0, current=0;
+        if(arr[0]==0){
+            return -1;
+        }
+        for(int i=0; i<arr.length; i++){
+            max = Math.max(max, i+arr[i]);
+            if(current==i){
+                if(i != arr.length-1){
+                    jump++;
+                }
+                current = max;
+            }else{
+                continue;
+            }
+        }
+        if(current<arr.length-1){
+                return -1;
+        }
+        return jump;
+    }
+}
